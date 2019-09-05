4df89b54b934019cc9c1be2f042a3be989648b45
deeem <karpov.dimon@gmail.com>
2019-09-05 13:27:53 +0300

    fetch post sigle post from redux storage

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import * as actions from '../../store/actions'
+import { connect } from 'react-redux'
 
 class Post extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params
+  }
+
   render() {
     const { id } = this.props.match.params
+    const { posts } = this.props
+    const post = posts.find(item => item.id === +id)
 
-    return <h1>View Post ({id})</h1>
+    return (
+      <>
+        {post && (
+          <>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </>
+        )}
+
+        <a href="/">Back to Posts List</a>
+      </>
+    )
   }
 }
 
-export default Post
+const mapStateToProps = state => {
+  return {
+    posts: state.posts.posts,
+    loading: state.posts.loading,
+  }
+}
+
+export default connect(mapStateToProps)(Post)
