1f9b3b69f6229a03e9712f317ef970ac0fa2db79
deeem <karpov.dimon@gmail.com>
2019-09-05 21:15:46 +0300

    Updates

---
+++ precache-manifest.c3eeb98866e09624e4e1ecf5265ebfcb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf9ea71e92150641a598d524369b457",
+    "url": "/simple-blog/index.html"
+  },
+  {
+    "revision": "38fa821ed9e1291b4bd0",
+    "url": "/simple-blog/static/css/main.6cfd5600.chunk.css"
+  },
+  {
+    "revision": "1f7ba9504c3c19e2107c",
+    "url": "/simple-blog/static/js/2.bc34815b.chunk.js"
+  },
+  {
+    "revision": "38fa821ed9e1291b4bd0",
+    "url": "/simple-blog/static/js/main.0f7b0823.chunk.js"
+  },
+  {
+    "revision": "5265e17e3d786aaa0000",
+    "url": "/simple-blog/static/js/runtime~main.b41541c1.js"
+  },
+  {
+    "revision": "b2eca4b209e55941dfb16427b646448f",
+    "url": "/simple-blog/static/media/iconfinder_close_309090.b2eca4b2.svg"
+  },
+  {
+    "revision": "fe42c42dd93e5543e5e5694b90d15c57",
+    "url": "/simple-blog/static/media/iconfinder_view-show_3671905.fe42c42d.svg"
+  },
+  {
+    "revision": "ab03a85fada3ae05d675f40402386885",
+    "url": "/simple-blog/static/media/iconfinder_write_126582.ab03a85f.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.a55b6ef55a3334a6fec3250b1b97ca9e.js"
+  "/simple-blog/precache-manifest.c3eeb98866e09624e4e1ecf5265ebfcb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/simple-blog/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
